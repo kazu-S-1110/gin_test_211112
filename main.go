ce0964438ef8d4412c7b76b8aa8d50f91256c36c
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-11-12 12:49:44 +0900

    User-Agentを取得してみた！

--- main.go
+++ main.go
@@ -8,9 +8,16 @@ import (
 
 func main() {
 	engine := gin.Default()
+	ua := ""
+	//use middleware
+	engine.Use(func(c *gin.Context) {
+		ua = c.GetHeader("User-Agent")
+		c.Next()
+	})
 	engine.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "hello, world!",
+			"message":    "hello, world!",
+			"User-agent": ua,
 		})
 	})
 	engine.Run(":3000")
